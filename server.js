5b31567df9e738c0cf01ad1e9a69d0733f519663
JamilYanezVasquez <jjyanez-vasquez@myseneca.ca>
2024-01-17 13:27:18 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // setup a 'root route' to listen on the default url path
 app.get("/", (req, res) => {
-    res.send(`<h2>Hello World!</h2>
+    res.send(`<h2>Jamil Yanez Vasquez - 139479232</h2>
     <a href="/about">About</a>`);
 });
 
