942fdd0091a3cb26599bded33411ae6fe7060c9f
Jamil Yanez Vasquez <jamilyanezvasquez@gmail.com>
2024-01-19 23:13:28 -0500

    Submission server.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Jamil Yanez Vasquez Student ID: 139479232 Date: 01/19/2024
+*
+*  Online (Cyclic) URL:  https://fantastic-erin-eagle.cyclic.app
+*
+********************************************************************************/ 
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
